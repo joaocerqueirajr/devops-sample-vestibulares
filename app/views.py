9bb967eb4b717bd3eff58cebebddb0dd65ab4c53
João Carlos <42190825+joaocerqueirajr@users.noreply.github.com>
2018-10-04 01:16:48 -0300

    Update views.py

--- app/views.py
+++ app/views.py
@@ -71,7 +71,9 @@ def criacao_vestibulares(request):
         {
             'title':'Criacao de Vestibulares',
             'cursos2': ['ADS' , 'Sistemas de Informacao', 'Administracao'],
-            'date':datetime.now(),    
+            'dia':datetime.now().day,
+            'mes':datetime.now().month,
+            'ano':datetime.now().year,
         })
 
     )
