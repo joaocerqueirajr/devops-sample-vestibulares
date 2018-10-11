b2c9cced8d64088f3f8d837194d6cc638b4f2202
Tiago Feliciano <42190570+tiagoff99@users.noreply.github.com>
2018-10-11 20:11:59 -0300

    Update 0001_initial.py

--- app/migrations/0001_initial.py
+++ app/migrations/0001_initial.py
@@ -1,9 +1,14 @@
 # -*- coding: utf-8 -*-
 from __future__ import unicode_literals
+  
  from django.db import models, migrations
+ 
+ 
  class Migration(migrations.Migration):
+  
      dependencies = [
     ]
+   
      operations = [
         migrations.CreateModel(
             name='Curso',
